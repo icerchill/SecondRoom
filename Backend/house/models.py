1ea487fc43337f5f8b5c11080fc558c7efb11f5c
zjason <jason.zhao1992@gmail.com>
2016-08-02 17:26:49 -0700

    user.html update

--- Backend/house/models.py
+++ Backend/house/models.py
@@ -2,7 +2,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 from user_info.models import user
-from time import time
 
 # Create your models here.
 def upload_location(instance, filename):
